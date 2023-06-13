0bfca6c99bfda1118559ad3b159e074ea7c08e9e
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2023-06-13 10:04:21 -0300

    v6 lol

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-
- //let d = new Date();
-  //alert("Today's date is " + d);
-
+let pos = 50;
+let pos2 = 40;
+let multiplier = 5;
+let angle = 0;
   function execute()
   {
     //document.getElementById("myDiv").style.opacity = 0;
-    //document.getElementById("myDiv").style.animation = "10s linear 0s infinite alternate loop1";
-    alert("lol ");
-    setInterval(animation, 0.2);
+    //alert("lol ");
+    //const box = document.getElementById('myDiv');
+    //box.style.position = 'absolute';
+    //box.style.top = '150px';
+    setInterval(animation, 10); //a função está em milisegundos, 1000 milisegundos é 1 minuto
     }
     function animation()
     {
-        let thing = document.getElementById("myDiv");
-        let frames = 0
-        let posY = thing.style.top;
-        let posX = thing.style.left;
 
-        posY += 0.1;
-        thing.style.top = posY + 'px';
-        thing.style.left = posX + 'px';
-        frames += 1;
+
+
+      //alert("ola");
+      //const box = document.getElementById('myDiv');
+      //box.style.position = 'relative';
+      const elem = document.getElementById("myDiv");
+      multiplier -= 0.2
+      angle -= 0.69;
+      pos+= -1 * multiplier;
+      pos2 += 1;
+      elem.style.top = pos + 'px';
+      elem.style.left= pos2 + 'px';
+      console.log(elem.style.top);
+      elem.style.transform = 'rotate(' + angle + '90deg)';
     }
+    
