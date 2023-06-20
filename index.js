47979156c046a7b7e412866d0cff6fdc7866920d
Matheus Hideki <104567107+Matheus-Hideki@users.noreply.github.com>
2023-06-19 23:07:07 -0300

    v7
    added some extra stuff to the code

--- index.js
+++ index.js
@@ -1,32 +1,37 @@
-let pos = 50;
-let pos2 = 40;
+
+let posY = 50;
+let posX = 40;
 let multiplier = 5;
 let angle = 0;
+
+let mouseX = 0;
+let mouseY = 0;
+
   function execute()
   {
-    //document.getElementById("myDiv").style.opacity = 0;
-    //alert("lol ");
-    //const box = document.getElementById('myDiv');
-    //box.style.position = 'absolute';
-    //box.style.top = '150px';
     setInterval(animation, 10); //a função está em milisegundos, 1000 milisegundos é 1 minuto
-    }
-    function animation()
+  }
+
+
+  function animation()
     {
 
-
-
-      //alert("ola");
-      //const box = document.getElementById('myDiv');
-      //box.style.position = 'relative';
+      if(posY < 900)
+      {
       const elem = document.getElementById("myDiv");
       multiplier -= 0.2
       angle -= 0.69;
-      pos+= -1 * multiplier;
-      pos2 += 1;
-      elem.style.top = pos + 'px';
-      elem.style.left= pos2 + 'px';
-      console.log(elem.style.top);
+      posY+= -1 * multiplier;
+      posX += 1;
+      elem.style.top = posY + 'px';
+      elem.style.left= posX + 'px';
+      //console.log(elem.style.top);
+      //console.log(posY);
       elem.style.transform = 'rotate(' + angle + '90deg)';
+
+
+      } else {
+        const elem = document.getElementById("myDiv");
+        elem.style.opacity = 0;
+      }
     }
-    
